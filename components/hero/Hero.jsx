8655aed51ea951fea3d12f3e51b611c2d0d1efa7
Martin Padron <martinepadronv@gmail.com>
2022-12-26 17:56:36 -0400

    Agregando hero principal en rama martin

---
+++ components/hero/Hero.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div>
+      <p className="font-bold text-3xl text-red-400">Proyecto limpio</p>
+    </div>
+  );
+};
+
+export default Hero;
