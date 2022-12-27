1fed20818a76342b4edbe50b0be6713aea43b928
Francisco Ortega <ortegareal1999@gmail.com>
2022-12-27 10:49:07 -0300

    Push de Hello

--- components/hero/Hero.jsx
+++ components/hero/Hero.jsx
@@ -3,7 +3,8 @@ import React from "react";
 const Hero = () => {
   return (
     <div>
-      <p className="font-bold text-3xl text-red-400">Proyecto limpio</p>
+      <p className="font-bold text-3xl text-red"></p>
+      <p className="font-bold text-3xl text-red">Hello</p>
     </div>
   );
 };
