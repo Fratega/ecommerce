7b58850c64c4e3ca2819e339cc0b054ba86e7e27
Francisco Ortega <ortegareal1999@gmail.com>
2022-12-28 19:11:25 -0300

    Pantallazo incompleto, pero falta poco, primer push bien hecho

--- components/header/Navbar.jsx
+++ components/header/Navbar.jsx
@@ -1,9 +1,55 @@
 import React from "react";
+import Image from "next/image";
+import Logo from "../../Public/img/Logo.png";
+import { FiAlignJustify } from "react-icons/fi";
+import { ImSearch } from "react-icons/im";
+import { MdShoppingCart } from "react-icons/md";
 
 const Navbar = () => {
   return (
-    <header>
-      Soy el header
+    <header className="px-5 min">
+      <nav className="py-10 mb-12">
+        <ul className="flex items-center justify-between">
+          <li className="flex">
+            <a href="#">
+              <Image src={Logo} alt="" className="w-[3rem]" />
+            </a>
+          </li>
+          <li className="">
+            <a href="#">
+              <FiAlignJustify className="cursor-pointer text-4xl md:invisible" />
+            </a>
+          </li>
+          <li className="invisible lg:visible">
+            <a href="#">
+              <p>Home</p>
+            </a>
+          </li>
+          <li className="invisible lg:visible">
+            <a href="#">
+              <p>Catálogo</p>
+            </a>
+          </li>
+          <li className="invisible lg:visible">
+            <a href="#">
+              <p>Contacto</p>
+            </a>
+          </li>
+          <li className="invisible lg:visible">
+            <a href="#">
+              <p>Categorías</p>
+            </a>
+          </li>
+          <li className="flex space-x-[1rem]">
+            <a>
+              <MdShoppingCart className="cursor-pointer text-4xl invisible lg:visible" />
+            </a>
+            <a href="#">
+              <ImSearch className="cursor-pointer text-3xl invisible lg:visible" />
+            </a>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
--- components/hero/Hero.jsx
+++ components/hero/Hero.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Image from 'next/image'
+import Jordans from '../../Public/img/Jordans.png'
 
 const Hero = () => {
   return (
     <section>
-        Soy el hero
+        <div className='bg-red-600 w-100 h-80 relative'>
+          <Image src={Jordans} alt=""  className='' fill />
+        </div>
     </section>
   )
 }
